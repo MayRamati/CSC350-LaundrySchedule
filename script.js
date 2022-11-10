1bb4cecf0cdcc5dff8b3216b39e8079578b3aadf
robertcochice <116222812+robertcochice@users.noreply.github.com>
2022-11-10 01:25:33 +0000

    Checkpoint

---
+++ script.js
@@ -0,0 +1,10 @@
+
+   
+function test() {
+
+  let time = Date.now()
+  let user = document.getElementById("uname").value
+  let pass = document.getElementById("pword").value
+  console.log({ time, user, pass })
+   
+ }
