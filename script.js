75a1642e6a65f79ae663c7b8659bf9dbde9f6055
robertcochice <116222812+robertcochice@users.noreply.github.com>
2022-11-27 17:05:12 +0000

    add password page

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+function check() {
+  var apartment_number = document.getElementById("ATP").value;
+  var password = document.getElementById("pword").value;
+  if (apt == apartment_number && password == pword) {
+    alert("logged in successfully");
+  } else {
 
-   
+    alert("account doesnt exist");
+  }
+}
+
 function test() {
 
   let time = Date.now()
   let user = document.getElementById("uname").value
   let pass = document.getElementById("pword").value
   console.log({ time, user, pass })
-   
- }
+
+}
