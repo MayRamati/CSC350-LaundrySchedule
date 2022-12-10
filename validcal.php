263b82e9199aa947ecc63be24821aa988848c0a8
robertcochice <116222812+robertcochice@users.noreply.github.com>
2022-12-10 10:19:17 -0500

    Create validcal.php

---
+++ validcal.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="calstyle.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <script src="script.js"></script>
+
+</body>
+
+  
+
+  <p><form action = 'listing_av.php' method= 'post'><input type='submit' value = 'click here to view avaliable slots'></form></p>
+
+<div class="calendar">
+  <div class="calendar_title" style="display: flex; justify-content: center; align-items: center;">
+
+    
+
+
+  <header>
+    <h1>December 2022</h1>
+        <table>
+  <thead>
+    <tr>
+      <th class="headcol"></th>
+      <th>Mon</th>
+      <th>Tue</th>
+      <th>Wed</th>
+      <th>Thu</th>
+      <th class="today">Fri</th>
+      <th>Sat</th>
+      <th>Sun</th>
+    </tr>
+  </thead>
+  </table>
+
+<div class="wrap"> 
+  <table class="offset">
+
+  <tbody>
+    <tr>
+    
+    <table>
+      <tr>
+      <td class="headcol">00:00-02:00 AM</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+      <tr>
+      <td class="headcol">2:00-04:00 AM</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+      <tr>
+      <td class="headcol">04:00-06:00 AM</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+      <tr>
+      <td class="headcol">06:00-08:00 AM</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+      <tr>
+      <td class="headcol">08:00-10:00 AM</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+      <tr>
+      <td class="headcol">10:00 AM - 12:00 PM</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+      <tr>
+      <td class="headcol">12:00-02:00 PM</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+      <tr>
+      <td class="headcol">12:00-02:00 PM</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+      <tr>
+      <td class="headcol">12:00-02:00 PM</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+      <tr>
+      <td class="headcol">12:00-02:00 PM</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+      <tr>
+      <td class="headcol">12:00-02:00 PM</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+      <tr>
+      <td class="headcol">12:00-02:00 PM</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </header>
+
+<div class="wrap"> 
+  <table class="offset">
+
+  <tbody>
+    <tr>
+    
+    </table>
+</div>
+  
+</html>
